83f5a2f8b8949217d56ef1136982fad8e6e00565
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-12-05 01:00:30 +0200

    Streaming output

--- app/app.py
+++ app/app.py
@@ -1,21 +1,18 @@
 import os
 import json
-import sys
-import asyncio
 import pika
 import eventlet
 import socketio
-from flask import Flask, request, jsonify
+from flask import Flask
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain_experimental.llm_symbolic_math.base import LLMSymbolicMathChain
 from langchain.vectorstores import Pinecone
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.agents import AgentType, Tool, ConversationalAgent
+from langchain.agents import AgentType, Tool, ConversationalAgent, AgentExecutor
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.utilities import GoogleSearchAPIWrapper
 from langchain.agents import initialize_agent
-from flask_socketio import SocketIO
 from classes import StreamAgentAnswerCallbackHandler
 from dotenv import load_dotenv
 import pinecone
@@ -24,7 +21,7 @@ app = Flask(__name__)
 sio = socketio.Server(cors_allowed_origins='*')
 app = socketio.WSGIApp(sio, app)
 load_dotenv()
-
+eventlet.monkey_patch()
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 os.environ["GOOGLE_CSE_ID"] = os.getenv("GOOGLE_CSE_ID")
@@ -37,24 +34,13 @@ os.environ["RABBITMQ_USERNAME"] = os.getenv("RABBITMQ_USERNAME")
 os.environ["RABBITMQ_PASSWORD"] = os.getenv("RABBITMQ_PASSWORD")
 os.environ["RABBITMQ_VIRTUAL_HOST"] = os.getenv("RABBITMQ_VIRTUAL_HOST")
 
-
-# prompt = ChatPromptTemplate.from_template(
-#   """Use the following pieces of context to answer the question at the end. If you don"t know the answer, just say that you don"t know, don"t try to make up an answer.
-#   ----------------
-#   CONTEXT: {context}
-#   ----------------
-#   CHAT HISTORY: {chat_history}
-#   ----------------
-#   QUESTION: {question}
-#   ----------------
-#   Helpful Answer:"""
-# )
-
 llm = ChatOpenAI(
     openai_api_key=os.environ["OPENAI_API_KEY"],
-    model="gpt-3.5-turbo",
+    model="gpt-4-1106-preview",
+    # model="gpt-3.5-turbo",
     streaming=True,
-    temperature=0
+    temperature=0,
+    callbacks=[StreamAgentAnswerCallbackHandler(sio=sio)]
 )
 
 pinecone.init(
@@ -72,49 +58,38 @@ qa = RetrievalQA.from_chain_type(
 
 symbolic_math = LLMSymbolicMathChain.from_llm(llm=llm)
 
-math_toll = Tool(
-  name="Math Equations",
-  func=symbolic_math.run,
-  description=("use this when you need to solve math equations.")
+math_tool = Tool(
+    name="Math Equations",
+    func=symbolic_math.run,
+    description=(
+        "Use this tool when you encounter math equations that need solving. "
+        "It employs symbolic math to provide accurate solutions."
+    )
 )
 
-retriever_tool =  Tool(
-      name="Knowledge Base",
-      func=qa.run,
-      description=(
-          "use this tool when answering general knowledge queries to get "
-          "more information about the topic."
-      )
-  )
+retriever_tool = Tool(
+    name="Knowledge Base",
+    func=qa.run,
+    description=(
+        "Leverage this tool when responding to general knowledge queries. "
+        "It taps into a comprehensive knowledge base, enhancing your responses."
+    )
+)
 
 search = GoogleSearchAPIWrapper()
 search_tool = Tool(
     name="Google Search",
     func=search.run,
-    description=("use this tool to search google when you need up to date or real-time information."),
+    description=(
+        "Activate this tool to perform Google searches and obtain up-to-date, real-time information. "
+        "Ideal for staying current on the latest developments."
+    )
 )
 
-tools=[retriever_tool, search_tool, math_toll]
+tools=[retriever_tool, search_tool, math_tool]
 
-# prefix = """Have a conversation with a human, answering the following questions as best you can. Use the tools available to answer the question. You have access to the following tools: Knowledge Base, Google Search, Math Equations"""
-# """Begin!"
-
-# ----------------
-# CHAT HISTORY: {chat_history}
-# ----------------
-# QUESTION: {input}
-# ----------------
-# AGENT SCRATCHPAD:{agent_scratchpad}"""
-
-prefix = """Answer the following questions as best you can. You have access to the following tools:
-
-{tools}
-
-This is relevant chat history:
-
-{chat_history}
-
-Use the following format:
+prefix = """Answer the following questions as best you can like you have a conversation. You have access to the following tools:"""
+format_instructions = """Use the following format:
 
 Question: the input question you must answer
 Thought: you should always think about what to do
@@ -134,8 +109,9 @@ Question: {input}
 prompt = ConversationalAgent.create_prompt(
     tools,
     prefix=prefix,
+    format_instructions=format_instructions,
     suffix=suffix,
-    input_variables=["input", "chat_history", "agent_scratchpad"],
+    input_variables=["input", "agent_scratchpad"],
 )
 
 memory = ConversationBufferWindowMemory(
@@ -152,64 +128,35 @@ agent = initialize_agent(
     max_iterations=3,
     early_stopping_method="generate",
     memory=memory,
-    return_intermediate_steps=False
+    handle_parsing_errors=True,
+    return_intermediate_steps=False,
+    agent_kwargs={
+        'prefix': prefix, 
+        'format_instructions': format_instructions,
+        'suffix': suffix
+    }
 )
 
-async def run_call(query: str, stream_it: StreamAgentAnswerCallbackHandler):
-    agent.agent.llm_chain.llm.callbacks = [stream_it]
-    # now query
-    return await agent.acall(inputs={"input": query})
+def callback(ch, method, properties, body):
+    sio.emit("add_entry")
+    eventlet.sleep(0)
 
-async def create_gen(query: str, stream_it: StreamAgentAnswerCallbackHandler):
-    task = asyncio.create_task(run_call(query, stream_it))
-    async for token in stream_it.aiter():
-        print(token)
-        sio.emit("next_token", {"token": token})
-        eventlet.sleep(0)
-    return await task
-
-# query=None
-# while True:
-#   if not query:
-#     query = input("Prompt: ")
-#   if query in ["quit", "q", "exit"]:
-#     sys.exit()
-#   result = agent(query)
-#   print(result)
-
-#   query = None
-
-# @app.route("/ask", methods=['POST'])
-# def run():
-#     data = request.json
-#     query_text = data.get('query', '')
-#     result = agent(query_text)
-
-#     return jsonify(result), 200
-
-async def callback(ch, method, properties, body):
     json_string = body.decode('utf-8')
     data = json.loads(json_string)
-    stream_it = StreamAgentAnswerCallbackHandler()
-    result = await create_gen(data["Question"], stream_it)
-    # result = agent()
+
+    result = agent(data["Question"])
     print(result)
 
-def callback_wrapper(ch, method, properties, body):
-    asyncio.run(callback(ch, method, properties, body))
-
-# Set up RabbitMQ connection
 credentials = pika.PlainCredentials(os.environ["RABBITMQ_USERNAME"], os.environ["RABBITMQ_PASSWORD"])
+
 def consume_messages():
     connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ["RABBITMQ_HOSTNAME"], 5672, os.environ["RABBITMQ_VIRTUAL_HOST"], credentials))
         
     channel = connection.channel()
 
-    # Declare the queue
     channel.queue_declare(queue='generate_question', durable=True, exclusive=False, auto_delete=True)
 
-    # Set up consumer
-    channel.basic_consume(queue='generate_question', on_message_callback=callback_wrapper, auto_ack=True)
+    channel.basic_consume(queue='generate_question', on_message_callback=callback, auto_ack=True)
 
     print('Waiting for messages. To exit, press CTRL+C')
     channel.start_consuming()
@@ -219,6 +166,5 @@ def run_consumer():
 
 if __name__ == "__main__":
     run_consumer() 
-
     eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 3000)), app)   
     
--- app/classes.py
+++ app/classes.py
@@ -1,9 +1,9 @@
 from enum import Enum
-from typing import Any, Coroutine, Dict, List, Optional
+import eventlet
+from typing import Any, Dict, List
 from uuid import UUID
-from langchain.callbacks.streaming_aiter import AsyncIteratorCallbackHandler
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain_core.messages import BaseMessage
-
 
 class FileType(Enum):
     PDF = 1
@@ -11,47 +11,33 @@ class FileType(Enum):
     DOCX = 3
     HTML = 4
     TXT = 5
-
-# class StreamAgentAnswerCallbackHandler(AsyncFinalIteratorCallbackHandler):
-#     def __init__(
-#         self,
-#         socket
-#     ):
-#         self.socket = socket
-        
-#     async def on_chat_model_start(
-#         self
-#     ):
-#         await self.socket.emit("new_entry")
-
-#     async def on_llm_new_token(self, token: str):
-#         await self.socket.emit('next_token', {'token': token})
-
-class StreamAgentAnswerCallbackHandler(AsyncIteratorCallbackHandler):
+class StreamAgentAnswerCallbackHandler(BaseCallbackHandler):
     content: str = ""
     final_answer: bool = False
     
-    def __init__(self) -> None:
+    def __init__(self, sio) -> None:
         super().__init__()
+        self.sio = sio
 
-    def on_chat_model_start(self, serialized: Dict[str, Any], messages: List[List[BaseMessage]], *, run_id: UUID, parent_run_id: UUID | None = None, tags: List[str] | None = None, metadata: Dict[str, Any] | None = None, **kwargs: Any) -> Coroutine[Any, Any, Any]:
+    def on_chat_model_start(self, serialized: Dict[str, Any], messages: List[List[BaseMessage]], *, run_id: UUID, parent_run_id: UUID | None = None, tags: List[str] | None = None, metadata: Dict[str, Any] | None = None, **kwargs: Any) -> Any:
         pass
-
-    async def on_llm_new_token(self, token: str, **kwargs) -> None:
+        
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
         self.content += token
-        # if we passed the final answer, we put tokens in queue
         if self.final_answer:
             if '"action_input": "' in self.content:
-                if token not in ['"', "}"]:
-                    self.queue.put_nowait(token)
+                if token not in ['"', "}","}\n","```"]:
+
+                    print(f"@{token}@")
+                    self.sio.emit("next_token", {"token": token})
+                    eventlet.sleep(0)
         elif "Final Answer" in self.content:
             self.final_answer = True
             self.content = ""
     
-    async def on_llm_end(self, response, **kwargs) -> None:
+    def on_llm_end(self, response, **kwargs) -> None:
         if self.final_answer:
             self.content = ""
             self.final_answer = False
-            self.done.set()
         else:
             self.content = ""
